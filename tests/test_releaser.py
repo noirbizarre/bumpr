84ac26508a9bee08e399410507644f7a743cb234
Axel Haustant <noirbizarre@gmail.com>
2013-08-21 20:18:55 +0200

    Start testing releaser

--- tests/test_releaser.py
+++ tests/test_releaser.py
@@ -4,9 +4,28 @@ try:
 except:
     import unittest
 
+from bumpr.config import Config
 from bumpr.releaser import Releaser
-
+from bumpr.version import Version
 
 
 class ReleaserTest(unittest.TestCase):
-    pass
+    def test_constructor(self):
+        config = Config({
+            'module': 'bumpr'
+        })
+        releaser = Releaser(config)
+
+        self.assertEqual(releaser.module.__name__, 'bumpr')
+        self.assertIsNotNone(releaser.module_file)
+        self.assertIsInstance(releaser.prev_version, Version)
+        self.assertIsInstance(releaser.version, Version)
+        self.assertIsInstance(releaser.next_version, Version)
+
+        self.assertIsNone(releaser.timestamp)
+
+        self.assertFalse(hasattr(releaser, 'vcs'))
+        self.assertFalse(hasattr(releaser, 'diffs'))
+
+        self.assertEqual(releaser.modified, set())
+        self.assertEqual(releaser.hooks, [])
