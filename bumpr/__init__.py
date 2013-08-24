30dbb91369ee42e8c72424ec80edfb9054edd389
Axel Haustant <noirbizarre@gmail.com>
2013-08-24 20:04:12 +0200

    Update to version 0.2.1.dev for next development cycle

--- bumpr/__init__.py
+++ bumpr/__init__.py
@@ -12,7 +12,7 @@ Bump'R: Version bumper and Python package releaser
 - Extensible with hooks
 '''
 
-__version__ = '0.2.0'
+__version__ = '0.2.1.dev'
 __description__ = 'Version bumper and Python package releaser'
 
 
