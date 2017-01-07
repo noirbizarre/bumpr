22d53669a1deee3c558f7d07cdd4f430d0279980
Axel Haustant <noirbizarre@gmail.com>
2017-01-07 14:18:18 +0100

    Migrate forgotten test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,10 +3,7 @@ from __future__ import print_function, unicode_literals
 
 import sys
 
-try:
-    import unittest2 as unittest
-except:
-    import unittest
+import pytest
 
 from mock import patch, call
 from subprocess import CalledProcessError
@@ -18,23 +15,23 @@ IS_PY3 = sys.version_info[0] == 3
 
 @patch('bumpr.helpers.check_output')
 @patch('subprocess.check_call')
-class ExecuteTest(unittest.TestCase):
+class ExecuteTest(object):
     def test_execute_quiet(self, check_call, check_output):
         output = 'some output'
         check_output.return_value = output
-        self.assertEqual(execute('some command'), output)
+        assert execute('some command') == output
         check_output.assert_called_with(['some', 'command'])
-        self.assertFalse(check_call.called)
+        assert not check_call.called
 
     def test_execute_verbose(self, check_call, check_output):
         execute('some command', verbose=True)
         check_call.assert_called_with(['some', 'command'])
-        self.assertFalse(check_output.called)
+        assert not check_output.called
 
     def test_execute_array(self, check_call, check_output):
         execute(['some', 'command'])
         check_output.assert_called_with(['some', 'command'])
-        self.assertFalse(check_call.called)
+        assert not check_call.called
 
     def test_execute_quoted(self, check_call, check_output):
         execute('some command "with quote"')
@@ -50,8 +47,8 @@ class ExecuteTest(unittest.TestCase):
 
     def test_execute_dry(self, check_call, check_output):
         execute('some command', dryrun=True)
-        self.assertFalse(check_call.called)
-        self.assertFalse(check_output.called)
+        assert not check_call.called
+        assert not check_output.called
 
     def test_execute_multiple(self, check_call, check_output):
         execute('''
@@ -63,7 +60,8 @@ class ExecuteTest(unittest.TestCase):
             call(['some', 'command']),
             call(['another', 'command']),
         )
-        self.assertSequenceEqual(check_output.call_args_list, expected)
+        for executed, expected in zip(check_output.call_args_list, expected):
+            assert executed == expected
 
     def test_execute_multiple_array(self, check_call, check_output):
         execute((
@@ -75,14 +73,15 @@ class ExecuteTest(unittest.TestCase):
             call(['some', 'command']),
             call(['another', 'command']),
         )
-        self.assertSequenceEqual(check_output.call_args_list, expected)
+        for executed, expected in zip(check_output.call_args_list, expected):
+            assert executed == expected
 
     def test_execute_error_quiet(self, check_call, check_output):
         error = CalledProcessError(1, 'cmd')
         error.output = 'some output'
         check_output.side_effect = error
 
-        with self.assertRaises(BumprError):
+        with pytest.raises(BumprError):
             to_patch = '{0}.print'.format('builtins' if IS_PY3 else '__builtin__')
             with patch(to_patch):
                 execute('some failed command')
@@ -90,5 +89,5 @@ class ExecuteTest(unittest.TestCase):
     def test_execute_error_verbose(self, check_call, check_output):
         check_call.side_effect = CalledProcessError(1, 'cmd')
 
-        with self.assertRaises(BumprError):
+        with pytest.raises(BumprError):
             execute('some failed command', verbose=True)
