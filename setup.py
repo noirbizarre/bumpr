3e57035041d0354661e9049d48aebf1e78d50711
Axel Haustant <noirbizarre@gmail.com>
2017-01-07 14:40:34 +0100

    Reuse develop.pip in Travis

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ install_requires = []
 tests_require = ['mock', 'pytest']
 doc_require = ['sphinx']
 qa_require = ['pytest-cov', 'coverage']
-dev_require = ['invoke']
 
 if sys.version_info[0:2] < (2, 7):
     install_requires.append('argparse')
@@ -74,7 +73,6 @@ setup(
         'test': tests_require,
         'doc': doc_require,
         'qa': qa_require,
-        'dev': dev_require,
     },
     keywords='version bump release tag',
     classifiers=[
